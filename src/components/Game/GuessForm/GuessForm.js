35fba8f65fcc08fb5cca30601167a59fcaae9493
Patricia Rozada <patricia.rozada@splashthat.com>
2023-11-18 20:33:24 +0100

    Add GuessForm

---
+++ src/components/Game/GuessForm/GuessForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function GuessForm() {
+  const [guess, setGuess] = React.useState('');
+  const handleInputChange = value => {
+    const uppercasedValue = !!value.length && value.toUpperCase();
+    setGuess(uppercasedValue);
+  }
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(guess);
+    setGuess('');
+  }
+  return <form onSubmit={(e) => handleFormSubmit(e)}>
+    <label htmlFor='guess-input'>Enter guess</label>
+    <input id='guess-input' type="text" value={guess} onChange={e => handleInputChange(e.target.value)} maxLength={5} minLength={5} required={true}  pattern="[a-zA-Z]{5}"/>
+  </form>;
+}
+
+export default GuessForm;
